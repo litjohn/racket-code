adb9a42b00b339c56e34397c1c578a6b2a50974d
litjohn <rong7ming@gmail.com>
2025-10-25 22:15:01 +0800

    del some trash

--- type.scm
+++
@@ -1,7 +0,0 @@
-(define-syntax type-val
-    (syntax-rules ()
-        ((_ type val) (cons type val))))
-
-(define-syntax is-this-type?
-    (syntax-rules ()
-        ((_ type val) (and (pair? val) (symbol? type) (eq? type (car val))))))
